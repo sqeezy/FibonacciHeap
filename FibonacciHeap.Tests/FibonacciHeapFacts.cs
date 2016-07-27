87eab764c92a4ec2c091dd843b18205f9868e326
sqeezy <sqeezy@gmx.de>
2016-07-27 23:26:26 +0200

    Revert "Cleanup of references."
    This reverts commit 7f9df62f57e6b20a7f886f13b0374e655da7c377.

--- FibonacciHeap.Tests/FibonacciHeapFacts.cs
+++ FibonacciHeap.Tests/FibonacciHeapFacts.cs
@@ -1,4 +1,11 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FibonacciHeap;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FibonacciHeap.Tests
 {
