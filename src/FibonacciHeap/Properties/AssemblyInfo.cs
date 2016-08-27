938fa98698814a39461d4baeee3187eeb5af630f
sqeezy <sqeezy@gmx.de>
2016-08-28 01:10:23 +0200

    Added nuget package files.

--- src/FibonacciHeap/Properties/AssemblyInfo.cs
+++ src/FibonacciHeap/Properties/AssemblyInfo.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("FibonacciHeap")]
 [assembly: AssemblyDescription("Simple implementation of the Fibonacci-Heap-Structure")]
+[assembly: AssemblyCompanyAttribute("Anton Herzog")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -18,5 +19,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
