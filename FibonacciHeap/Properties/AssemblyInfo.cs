e68d945bee3c0285e2853b934e706930f66f0096
sqeezy <sqeezy@gmx.de>
2016-08-27 10:27:24 +0200

    Revert "Migrated from packages.config to project.json."
    This reverts commit c9e6dc36bf6aed95a65eb38dc20511b4464cdb9f.

--- FibonacciHeap/Properties/AssemblyInfo.cs
+++ FibonacciHeap/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FibonacciHeap")]
-[assembly: AssemblyDescription("Implementation of the Fibonacci-Heap-Structure")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("FibonacciHeap")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
