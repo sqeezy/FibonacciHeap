c9e6dc36bf6aed95a65eb38dc20511b4464cdb9f
sqeezy <sqeezy@gmx.de>
2016-08-27 10:27:24 +0200

    Migrated from packages.config to project.json.

--- FibonacciHeap/Properties/AssemblyInfo.cs
+++ FibonacciHeap/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FibonacciHeap")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Implementation of the Fibonacci-Heap-Structure")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FibonacciHeap")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
