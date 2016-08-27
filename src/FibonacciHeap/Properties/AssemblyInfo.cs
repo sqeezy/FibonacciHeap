f7ef7b435ededfd4655564be76c8c427e5da3360
sqeezy <sqeezy@gmx.de>
2016-08-28 00:27:50 +0200

    Assembly definition cleanup.

--- src/FibonacciHeap/Properties/AssemblyInfo.cs
+++ src/FibonacciHeap/Properties/AssemblyInfo.cs
@@ -6,18 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FibonacciHeap")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("FibonacciHeap")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Simple implementation of the Fibonacci-Heap-Structure")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4f7ad55d-acea-468f-ad23-1e03ddae475e")]
