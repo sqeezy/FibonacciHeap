2bb32046d8c04fb584410704104adbd186b5ef03
Alexey <alexey@azelenin.work>
2016-11-16 23:17:47 -0500

    Renamed the new classes with generic key types. Generated the classes with "double" key type with same names as were used before.

--- src/FibonacciHeap/FibonacciHeapNode.cs
+++ src/FibonacciHeap/FibonacciHeapNode.cs
@@ -1,22 +1,9 @@
 ﻿namespace FibonacciHeap
 {
-    public class FibonacciHeapNode<T, TKey> where TKey: System.IComparable
+    public class FibonacciHeapNode<T> : FibonacciHeapNodeGenericKey<T, double>
     {
-        public FibonacciHeapNode(T data, TKey key)
+        public FibonacciHeapNode( T data, double key ) : base( data, key )
         {
-            Right = this;
-            Left = this;
-            Data = data;
-            Key = key;
         }
-
-        public T Data { get; }
-        public FibonacciHeapNode<T, TKey> Child { get; set; }
-        public FibonacciHeapNode<T, TKey> Left { get; set; }
-        public FibonacciHeapNode<T, TKey> Parent { get; set; }
-        public FibonacciHeapNode<T, TKey> Right { get; set; }
-        public bool Mark { get; set; }
-        public TKey Key { get; set; }
-        public int Degree { get; set; }
     }
 }
