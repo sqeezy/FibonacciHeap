04b8271c2ecc72039fae988faa4f9f9b97bbdc77
Anton Herzog <info@antonherzog.com>
2019-01-15 17:09:27 +0100

    Small Fixes (#14)
    * Refactor out constant value.
    This way its not in every generic instance of the heap.
    
    * Make Constants.cs internal.

---
+++ src/FibonacciHeap/Constants.cs
@@ -0,0 +1,9 @@
+using System;
+
+internal static class Constants
+{
+        /// <summary>
+        /// Maximum nodes quantity in the heap.
+        /// </summary>
+        public static readonly double OneOverLogPhi = 1.0/Math.Log((1.0 + Math.Sqrt(5.0))/2.0);
+}
